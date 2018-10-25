2203a0b264d0103e008c994cc36272a36b86acb6
mrzhangcc <cczhang1987@hotmail.com>
2018-10-25 17:27:39 +0800

    Big Change

---
+++ application/third_party/eapp/config.php
@@ -0,0 +1,21 @@
+<?php
+define('DIR_ROOT', dirname(__FILE__).'/');
+define("OAPI_HOST", "https://oapi.dingtalk.com");
+//Suite
+define("APP_KEY_DELIVERY", "dingy941wgaj44ss343s");
+define("APP_SECRET_DELIVERY", "kKte7iRi_5-FuHPJN32r9MPScw2dmWyli7whx9Omdjg_38-PhyBgLnVv1eX2SS68");
+define('APP_AGENT_DELIVERY', '194730870');
+
+define("APP_KEY_STORAGE", "dingy941wgaj44ss343s");
+define("APP_SECRET_STORAGE", "kKte7iRi_5-FuHPJN32r9MPScw2dmWyli7whx9Omdjg_38-PhyBgLnVv1eX2SS68");
+define('APP_AGENT_STORAGE', '194730870');
+
+define("APP_KEY_FINANCE", "ding41ifvr3lqfhhdsyi");
+//define("APP_KEY_FINANCE", "ding1aarbzvitbuk9sz5");
+define("APP_SECRET_FINANCE", "FHG9bvLpcoXYXS3N7TgzoMZqCjzmAnBL8ZNxrlomBwnH4O2SE4RFQGrM4sDHF-fb");
+//define("APP_SECRET_FINANCE", "vRd9ED29aqemba-00hCV4owyUImjpqfI7DADS39xctxctGTtYbGBR4MPzxtVB8ky");
+define('APP_AGENT_FINANCE', '193911597');
+//define('APP_AGENT_FINANCE', '195584881');
+
+define('CORPID', 'dingcf233e1ad424b5fa35c2f4657eb6378f');
+//define('CORPID', 'ding0a679856ef28f52235c2f4657eb6378f');
---
+++ application/third_party/eapp/login.php
@@ -0,0 +1,56 @@
+<?php
+require_once(__DIR__ . "/config.php");
+require_once(__DIR__ . "/util/Http.php");
+require_once(__DIR__ . "/util/Log.php");
+
+
+function getAccessToken($appkey, $appsecret) {
+    $ret = Http::get("/gettoken",
+    array(
+        "appkey" => $appkey,
+        "appsecret" => $appsecret,
+    ));
+
+    if ($ret->errcode != 0) {
+        return false;
+    } 
+
+    return $ret->access_token;
+}
+
+function getUserId () {
+    $app = $_POST['app'];
+    if ($app == 'finance') {
+        $access_token = getAccessToken(APP_KEY_FINANCE, APP_SECRET_FINANCE);
+    } else {
+        $access_token = false;
+    }
+
+    if ($access_token) {
+        $code = $_POST['authCode'];
+        // 通过authcode和accesstoken获取哦用户信息
+        $res = Http::get("/user/getuserinfo",
+            array(
+                "access_token" => $access_token,
+                "code" => $code,
+            ));
+        if ($res->errcode == 0) {
+            $user = Http::get("/user/get",
+                array(
+                    "access_token" => $access_token,
+                    "userid" => $res->userid,
+                ));
+            if ($res->errcode == 0) {
+                return array(
+                    'user_id' => $res->userid,
+                    'user_name' => $user->name
+                );
+            } else {
+                Log::e('获取用户信息错误，'.$user->errmsg);
+            }
+        } else {
+            Log::e('获取用户ID错误，'.$res->errmsg);
+        }
+    }
+    return false;
+}
